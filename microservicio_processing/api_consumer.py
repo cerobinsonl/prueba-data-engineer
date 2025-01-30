bff6babf0dd9be8e4e85aed8387a2b2e3657838a
cerobinsonl <60712960+cerobinsonl@users.noreply.github.com>
2025-01-30 01:09:34 -0500

    Error handling in api_consumer function

--- microservicio_processing/api_consumer.py
+++ microservicio_processing/api_consumer.py
@@ -4,21 +4,23 @@ API_URL = "https://api.postcodes.io/postcodes"
 
 def fetch_postcode(latitude: float, longitude: float):
     url = API_URL+f"?lon={longitude}&lat={latitude}"
-    response = requests.get(url)
-    
-    if response.status_code != 200:
-        print(f"Error en API externa: {response.status_code} - {response.text}")
-        return None
-    
-    data = response.json()
+    try:
+        response = requests.get(url,timeout=5)
+        
+        if response.status_code != 200:
+            print(f"Error en API externa: {response.status_code} - {response.text}")
+            return None
+        
+        data = response.json()
 
-    print(f"Respuesta API: {data}")
+        print(f"Respuesta API: {data}")
 
-    if isinstance(data["result"], list) and len(data["result"]) > 0:
-        return data["result"][0].get("postcode", None)
+        if isinstance(data["result"], list) and len(data["result"]) > 0:
+            return data["result"][0].get("postcode", None)
+        
+        if isinstance(data["result"], dict):
+            return data["result"].get("postcode", None)
     
-    if isinstance(data["result"], dict):
-        return data["result"].get("postcode", None)
-
-    print("Estructura inesperada en la respuesta de la API.")
+    except Exception as e:
+        print(f"Estructura inesperada en la respuesta de la API: {e}")
     return None
